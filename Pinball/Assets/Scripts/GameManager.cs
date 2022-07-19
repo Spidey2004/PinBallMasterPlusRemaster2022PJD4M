ba0679f1af4f58c949e2a40030231b728365ecd2
Edson de Carvalho Ferreira Filho <filho.ferreira@academico.ifrn.edu.br>
2022-07-19 10:24:51 -0300

    ObserverPattern #2

--- Pinball/Assets/Scripts/GameManager.cs
+++ Pinball/Assets/Scripts/GameManager.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Classe usada para gerenciar o jogo
+/// </summary>
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private string guiName; //nome da fase da interface
+    [SerializeField] private string levelName;
+    [SerializeField] private GameObject playerAndCameraPrefab;
+
     void Start()
     {
-        
-    }
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.LoadScene(guiName);
+        SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive).completed += operation =>
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
+        };
+        Vector3 playerStartPosition = GameObject.Find("PlayerStart").transform.position;
         
+        Scene levelScene = default;
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if (SceneManager.GetSceneAt(i).name == levelName)
+            {
+                levelScene = SceneManager.GetSceneAt(i);
+                break;
+            }
+        }
+
+        if (levelScene != default) SceneManager.SetActiveScene(levelScene);
+
+        Instantiate(playerAndCameraPrefab, playerStartPosition, Quaternion.identity);
     }
 }
--- Pinball/Assets/Scripts/WobbleText.cs
+++ Pinball/Assets/Scripts/WobbleText.cs
@@ -7,15 +7,14 @@ using UnityEngine;
 public class WobbleText : MonoBehaviour
 {
     public TMP_Text textComponent;
-
-    private void Update()
+    void Update()
     {
         textComponent.ForceMeshUpdate();
         var textInfo = textComponent.textInfo;
 
         for (int i = 0; i < textInfo.characterCount; ++i)
         {
-            var charInfo = textInfo.characterCount[i];
+            var charInfo = textInfo.characterInfo[i];
 
             if (!charInfo.isVisible)
             {
@@ -27,7 +26,7 @@ public class WobbleText : MonoBehaviour
             for (int j = 0; j < 4; ++j)
             {
                 var orig = verts[charInfo.vertexIndex + j];
-                verts[charInfo.vertexIndex + j] = orig + new Vector3(0, Mathf.Sin(Time.time * 2f + orig.x * 0));
+                verts[charInfo.vertexIndex + j] = orig + new Vector3(0, Mathf.Sin(Time.time * 2f + orig.x * 0.01f) * 10f, 0);
             }
 
             
